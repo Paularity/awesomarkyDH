76cae03a7afd32503a0cc61c0466ca08797d76e9
Christian Paul Decembrana <christiandecembrana1@gmail.com>
2018-01-08 15:04:15 +0800

    commited 3:04PM

--- resources/views/frontend/doctors.blade.php
+++ resources/views/frontend/doctors.blade.php
@@ -1,4 +1,70 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="row">
+  <div class="col-md-6 text-content">
+    <h2> <strong>Meet Our Professional Team</strong> </h2>
+    <div style="border-bottom:2px solid #56B0D2;width:70px"></div>
+    <h4>Ut auctor nulla ac tellus tempor pulvinar. Integer facilisis dapibus ante eu interdum. Cras ligula augue, rutrum at lacus et, viverra porta urna. Curabitur maximus erat mi. Integer ultricies pulvinar tempus. Cras scelerisque pulvinar quam in lacinia. Duis gravida eu enim in fermentum. Aenean tempor iaculis commodo. Integer nec sem sit amet felis vulputate rhoncus. Sed ante nunc, dictum vel libero in.</h4>
+  </div>
+  <div class="col-md-6">
+    <img src="images/slide3.jpg" alt="Docs" style="height: 100%; width: 100%; background-size: cover">
+  </div>
+</div>
+
+<div class="containerB">
+  <div class="row text-content">
+    <h2><strong>Medical Specialists</strong></h2>
+    <div style="border-bottom:2px solid #56B0D2;width:70px"></div>
+  </div>
+
+  <div class="row">
+    <div class="col-md-3 hover-doctor" style="padding:3%">
+      <div class="row">
+        <img src="images/doc1.jpg" alt="Docs" class="doc-pic">
+      </div>
+      <div class="row text-center">
+        <p><small>CARDIAC SURGEON</small></p>
+        <h4>Dr. Marco Rossi</h4>
+        <p> <span><i class="fa fa-mobile" aria-hidden="true"></i></span> <small>+ 800 123 45 67</small></p>
+        <p> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> <small>example@awesmarky.com</small></p>
+      </div>
+    </div>
+    <div class="col-md-3 hover-doctor" style="padding:3%">
+      <div class="row">
+        <img src="images/doc2.png" alt="Docs" class="doc-pic">
+      </div>
+      <div class="row text-center">
+        <p><small>PEDIATRECIAN</small></p>
+        <h4>Dr. Jane Doe</h4>
+        <p> <span><i class="fa fa-mobile" aria-hidden="true"></i></span> <small>+ 800 123 45 67</small></p>
+        <p> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> <small>example@awesmarky.com</small></p>
+      </div>
+    </div>
+    <div class="col-md-3 hover-doctor" style="padding:3%">
+      <div class="row">
+        <img src="images/doc3.jpg" alt="Docs" class="doc-pic">
+      </div>
+      <div class="row text-center">
+        <p><small>DENTIST</small></p>
+        <h4>Dr. Lee Sin</h4>
+        <p> <span><i class="fa fa-mobile" aria-hidden="true"></i></span> <small>+ 800 123 45 67</small></p>
+        <p> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> <small>example@awesmarky.com</small></p>
+      </div>
+    </div>
+    <div class="col-md-3 hover-doctor" style="padding:3%">
+      <div class="row">
+        <img src="images/doc4.jpg" alt="Docs" class="doc-pic">
+      </div>
+      <div class="row text-center">
+        <p><small>MEDICAL DOCTOR</small></p>
+        <h4>Dr. Zed Black</h4>
+        <p> <span><i class="fa fa-mobile" aria-hidden="true"></i></span> <small>+ 800 123 45 67</small></p>
+        <p> <span><i class="fa fa-envelope" aria-hidden="true"></i></span> <small>example@awesmarky.com</small></p>
+      </div>
+    </div>
+  </div>
+
+</div>
+
 @endsection
--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -24,17 +24,17 @@ var slide = 1;
     slider();
 </script>
 
-<div class="containerA">
+<div class="containerA text-content">
   <h1>Welcome</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
 
-<div class="containerB">
+<div class="containerB text-content">
   <h1>Something here</h1>
   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 </div>
 
-<div class="containerA">
+<div class="containerA text-content">
   <div class="row">
     <div class="col-md-4 text-center">
       <div class="icon-wrapper"><i class="custom-icon"><img class="iconImg" src="images/lungs.png" alt=""> <span class="fix-editor"></span></i></div>
@@ -57,7 +57,7 @@ var slide = 1;
   </div>
 </div>
 
-<div class="containerB">
+<div class="containerB text-content">
   <h2>Testimonials</h2>
   <div class="row">
 
